40352407679135d091b8f7d80ade49f089084583
ajcorneillie <123045553+ajcorneillie@users.noreply.github.com>
2025-05-07 18:12:32 -0600

    1

---
+++ HorrorGame/Assets/EnemyCollider.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            Debug.Log("you lose");
+        }
+    }
+}
